971a0147964bdb1e297c03e7ec47d25ff917a629
marcoacsena <marcoacsena@gmail.com>
2021-05-02 17:18:39 -0300

    Implementação do código para tirar foto ou recuperar uma da galeria.

--- app/src/main/java/com/example/organizese/activity/LoginActivity.java
+++ app/src/main/java/com/example/organizese/activity/LoginActivity.java
@@ -1,13 +1,20 @@
 package com.example.organizese.activity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.organizese.R;
@@ -21,12 +28,25 @@ import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
+import static android.provider.MediaStore.ACTION_IMAGE_CAPTURE;
+
 public class LoginActivity extends AppCompatActivity {
 
+    private static final int SELECAO_CAMERA = 1;
+    private static final int SELECAO_GALERIA = 2;
+
     private EditText email, senha;
+    private ImageView ivFoto;
     private Button btnAcessar;
+    private Button btnCamera, ibtnGaleria;
     private Usuario usuario;
     private FirebaseAuth autenticacao = FirebaseAuth.getInstance();
+
+//    private String [] permissoesNecessarias = new String[] {
+//
+//            Manifest.permission.READ_EXTERNAL_STORAGE,
+//            Manifest.permission.CAMERA
+//    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +56,9 @@ public class LoginActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
         senha = findViewById(R.id.senha);
         btnAcessar = findViewById(R.id.btnAcessar);
+        ivFoto = findViewById(R.id.ivFoto);
+        btnCamera = findViewById(R.id.btnCamera);
+        ibtnGaleria = findViewById(R.id.ibtnGaleria);
 
         btnAcessar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,6 +79,55 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePicture = new Intent(ACTION_IMAGE_CAPTURE);
+                if(takePicture.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(takePicture, SELECAO_CAMERA);
+                }
+            }
+        });
+
+        ibtnGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent pickImage = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                if(pickImage.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(pickImage, SELECAO_GALERIA);
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            Bitmap imagem = null;
+
+        try {
+            switch (requestCode){
+                case SELECAO_CAMERA:
+                    imagem = (Bitmap) data.getExtras().get("data");
+                    break;
+                case SELECAO_GALERIA:
+                    Uri localImagemRecuperada = data.getData();
+                    imagem = MediaStore.Images.Media.getBitmap(getContentResolver(), localImagemRecuperada);
+                    break;
+            }
+
+            if(imagem != null){
+                ivFoto.setImageBitmap(imagem);
+            }
+
+        }catch (Exception e){e.printStackTrace();}
+
+        }
     }
 
     private Boolean validarCadastro(String emailUsuario, String senhaUsuario) {
